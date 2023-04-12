99e925d5d7b8343427e63b3013d920d28992b09b
yebin shin <pandayb1016@gmail.com>
2023-04-12 17:24:58 +0900

    chore router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/index.vue'
 import Signup from '../pages/member/signup.vue'
 import Login from '../pages/member/login.vue'
+import Modify from '../pages/member/modify.vue'
 import Map from '../pages/meeting/offMeeting/index.vue'
 import Meeting from '../pages/meeting/onMeeting/index.vue'
 
@@ -14,14 +15,19 @@ const router = createRouter({
       component: Home
     },
     {
-      path: '/member/signup',
+      path: '/members/signup',
       name: 'Signup',
       component: Signup
     },
     {
-      path: '/member/login',
+      path: '/members/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/members/modify',
+      name: 'Modify',
+      component: Modify
     },
     {
       path: '/map',
