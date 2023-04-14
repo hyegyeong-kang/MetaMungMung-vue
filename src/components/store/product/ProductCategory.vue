71c2ee47275604e687414514fc5156059fdbaac4
jeonga-Son <jjeong0106@gmail.com>
2023-04-14 23:20:26 +0900

    modifying product list design...

--- src/components/store/product/ProductCategory.vue
+++ src/components/store/product/ProductCategory.vue
@@ -1,110 +1,136 @@
 <template>
-  <div id="pc-categoryMenuWidget" class="_3Te07yM0Z_">
-    <div class="_1bplHci37r">
-      <div class="_3TZha2IPoQ">
-        <div class="kKWaaZzr6M">
-          <div class="_1Do44itO20">
-            <div class="_20owVxSw3I type_white_gnb _1Qz6L4zdcV">
-              <div class="_1AJ8D2PjS4 _3nO3wKj4-Z">
-                <div class="_1J2oAxZvAG" style="text-align: center">
-                  <ul class="_3AV7RVieRB">
-                    <span
-                      id="categoryList"
-                      class="_1E0Pj6SKQC type_white_gnb _3KXbOehKIC N=a:LNB.best"
-                    >
-                      <a href="/sajopet/best?cp=1" class="_3kz1USn-rs"
-                        >🏆베스트</a
-                      >
-                    </span>
-                    <span
-                      id="categoryList"
-                      class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                      data-category-menu-key="1006c60a4199496a80fabc6586df7376"
-                    >
-                      <a
-                        href="/sajopet/category/1006c60a4199496a80fabc6586df7376?cp=1"
-                        class="_3HQCww4jR6 _27UBX4PJLi"
-                        >⚡초특가 할인<span class="_3PK0aGa3Hc"></span
-                      ></a>
-                    </span>
-                    <span
-                      id="categoryList"
-                      class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                      data-category-menu-key="f28a4959a4a843ff9128a580ecccbff1"
-                    >
-                      <a
-                        href="/sajopet/category/f28a4959a4a843ff9128a580ecccbff1?cp=1"
-                        class="_3HQCww4jR6 _27UBX4PJLi"
-                        >🐶강아지 건식<span class="_3PK0aGa3Hc"></span
-                      ></a>
-                    </span>
-                    <span
-                      id="categoryList"
-                      class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                      data-category-menu-key="b07819b1ddb24db482bd74bc51aa7f32"
-                    >
-                      <a
-                        href="/sajopet/category/b07819b1ddb24db482bd74bc51aa7f32?cp=1"
-                        class="_3HQCww4jR6 _27UBX4PJLi"
-                        >🥫강아지 습식<span class="_3PK0aGa3Hc"></span
-                      ></a>
-                    </span>
-                    <span
-                      id="categoryList"
-                      class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                      data-category-menu-key="8d4831ae8f4c4605a9ecf3934495bd4f"
-                    >
-                      <a
-                        href="/sajopet/category/8d4831ae8f4c4605a9ecf3934495bd4f?cp=1"
-                        class="_3HQCww4jR6 _27UBX4PJLi"
-                        >🦴강아지 간식&amp;영양제<span
-                          class="_3PK0aGa3Hc"
-                        ></span
-                      ></a>
-                    </span>
-                    <span
-                      id="categoryList"
-                      class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                      data-category-menu-key="81337ba6dfc14b93aad5f2cef098f0e1"
-                    >
-                      <a
-                        href="/sajopet/category/81337ba6dfc14b93aad5f2cef098f0e1?cp=1"
-                        class="_3HQCww4jR6 _27UBX4PJLi"
-                        >😺고양이 건식<span class="_3PK0aGa3Hc"></span
-                      ></a>
-                    </span>
-                    <span
-                      id="categoryList"
-                      class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                      data-category-menu-key="672ece7ec6e14aef957bb35f8725d76f"
-                    >
-                      <a
-                        href="/sajopet/category/672ece7ec6e14aef957bb35f8725d76f?cp=1"
-                        class="_3HQCww4jR6 _27UBX4PJLi"
-                        >🥫고양이 습식<span class="_3PK0aGa3Hc"></span
-                      ></a>
-                    </span>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+  <div data-viewname="DGlobalSearchBandSubTabNavView" class="bandListWrap">
+    <hr />
+    <div class="wide -w840">
+      <div class="searchedSortBand">
+        <a
+          href=""
+          class="sortButton _btnTabNav active"
+          data-id="1"
+          :class="{ active: isActive }"
+          @click.prevent="cateToggle(0)"
+          >전체</a
+        >
+
+        <a
+          href=""
+          class="sortButton _btnTabNav"
+          data-id="2"
+          :class="{ active: isActive }"
+          @click.prevent="cateToggle(1)"
+          >사료</a
+        >
+
+        <a
+          href=""
+          class="sortButton _btnTabNav"
+          data-id="3"
+          :class="{ active: isActive }"
+          @click.prevent="cateToggle(2)"
+          >간식 & 건강</a
+        >
+
+        <a
+          href=""
+          class="sortButton _btnTabNav"
+          data-id="4"
+          :class="{ active: isActive }"
+          @click.prevent="cateToggle(3)"
+          >위생 & 배변</a
+        >
+
+        <a
+          href=""
+          class="sortButton _btnTabNav"
+          data-id="5"
+          :class="{ active: isActive }"
+          @click.prevent="cateToggle(4)"
+          >미용 & 목욕</a
+        >
+
+        <a
+          href=""
+          class="sortButton _btnTabNav"
+          data-id="6"
+          :class="{ active: isActive }"
+          @click.prevent="cateToggle(5)"
+          >의류 & 악세사리</a
+        >
+
+        <a
+          href=""
+          class="sortButton _btnTabNav"
+          data-id="7"
+          :class="{ active: isActive }"
+          @click.prevent="cateToggle(6)"
+          >장난감</a
+        >
       </div>
     </div>
+    <hr />
   </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const isActive = ref(false);
+    let prevIdx = 0;
+
+    const cateToggle = (idx) => {
+      const parent = document.getElementsByClassName("searchedSortBand")[0];
+      if (idx != prevIdx) {
+        parent.children[prevIdx].classList.remove("active");
+        parent.children[idx].classList.add("active");
+        prevIdx = idx;
+      }
+    };
+
+    return {
+      isActive,
+      cateToggle,
+    };
+  },
+};
 </script>
 
 <style scoped>
-ul > span {
-  width: calc(100% / 7);
+.bandListWrap {
+  position: relative;
+  text-align: center;
+  display: inline-block;
+  width: 100%;
 }
-
-ul > span > a {
-  font-size: 15px;
+.wide.-w840 {
+  width: 100%;
+}
+.wide {
+  width: 100%;
+  /* margin: 0 auto; */
+}
+.-w840 {
+  width: 100% !important;
+}
+.searchedSortBand {
+  text-align: center;
+  padding: 16px 0 14px;
+}
+.searchedSortBand .sortButton.active {
+  font-size: 13px;
+  font-weight: 600;
+  color: #444;
+  background: #e9eaea;
+}
+.searchedSortBand .sortButton {
+  display: inline-block;
+  border-radius: 14px;
+  padding: 0 20px 0 20px;
+  height: 28px;
+  line-height: 28px;
+  font-size: 13px;
+  font-weight: 400;
+  color: #888;
 }
 </style>
--- src/pages/store/product/index.vue
+++ src/pages/store/product/index.vue
@@ -9,36 +9,32 @@
         />
         <span style="color: #1f1f1f">멍멍스토어</span>
       </h1>
-      <!--일반-->
-      <div class="bn-line"></div>
-      <div class="top-line"></div>
 
-      <!-- 카테고리 -->
       <ProductCategory />
 
       <!--새로나온 친구들 리스트-->
       <div class="con prod-list hot-stuff">
         <nav class="prod-list-1">
-          <div class="prod-list-title"></div>
+          <div class="prod-list-title">멍멍스토어 상품들</div>
           <ul class="row">
             <li class="cell">
-              <a href="/products/1">
-                <div class="img-box">
-                  <span
-                    ><img
-                      src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FFRPBAPCAR8401_AW_00.jpg"
-                      alt=""
-                      class="img1"
-                  /></span>
-                </div>
-              </a>
-              <div class="title">차량용 급속충전기_어피치</div>
-              <div class="price"><span>20,000원</span></div>
+              <div class="img-box">
+                <span
+                  >prod-img-1<img
+                    src="https://images-dev.wefluffy.co.kr/product-option/1192/modify_detail_056.webp"
+                    alt=""
+                    class="img1"
+                /></span>
+              </div>
+              <div class="title">
+                프로플랜 센서티브 스킨 앤 스토막 어덜트 12kg
+              </div>
+              <div class="price"><span>87,000원</span></div>
             </li>
             <li class="cell">
               <div class="img-box">
                 <span
-                  ><img
+                  >prod-img-2<img
                     src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FFRPBRYCAR8400_AW_00.jpg"
                     alt=""
                     class="img2"
@@ -50,7 +46,7 @@
             <li class="cell">
               <div class="img-box">
                 <span
-                  ><img
+                  >prod-img-3<img
                     src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2F8809641461544_AW_00.jpg"
                     alt=""
                     class="img3"
@@ -62,8 +58,8 @@
             <li class="cell">
               <div class="img-box">
                 <span
-                  ><img
-                    src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FFRPBAPCAR8401_AW_00.jpg"
+                  >prod-img-4<img
+                    src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FAW_00_shade_TB.jpg"
                     alt=""
                     class="img4"
                 /></span>
@@ -71,8 +67,243 @@
               <div class="title">페이스형안대-리틀 튜브</div>
               <div class="price"><span>12,000원</span></div>
             </li>
+            <li class="cell">
+              <div class="img-box">
+                <span
+                  >prod-img-5<img
+                    src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FAW_00_littleRY_case.jpg"
+                    alt=""
+                    class="img5"
+                /></span>
+              </div>
+              <div class="title">에어팟케이스_리틀라이언</div>
+              <div class="price"><span>15,000원</span></div>
+            </li>
+            <li class="cell">
+              <div class="img-box">
+                <span
+                  >prod-img-6<img
+                    src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FFRPBRYHTL0001_AW_00.jpg"
+                    alt=""
+                    class="img6"
+                /></span>
+              </div>
+              <div class="title">핸드타올_별라이언</div>
+              <div class="price"><span>69,000원</span></div>
+            </li>
+            <li class="cell">
+              <div class="img-box">
+                <span
+                  >prod-img-7<img
+                    src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FAW_00_neckRY.jpg"
+                    alt=""
+                    class="img7"
+                /></span>
+              </div>
+              <div class="title">후드 목베개-리틀라이언</div>
+              <div class="price"><span>24,000원</span></div>
+            </li>
+            <li class="cell">
+              <div class="img-box">
+                <span
+                  >prod-img-8<img
+                    src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FAW_01_passport_AP.jpg"
+                    alt=""
+                    class="img8"
+                /></span>
+              </div>
+              <div class="title">투명여권지갑_리틀 어피치</div>
+              <div class="price"><span>8,000원</span></div>
+            </li>
           </ul>
         </nav>
+        <span
+          id="spanId"
+          @click="removeHideFunc"
+          style="margin-top: 50px"
+          class="more-prod hide"
+          >상품 더 보기</span
+        >
+        <ul id="ulId" class="row prod-list-1-2 hide">
+          <li class="cell">
+            <div class="img-box">
+              <span
+                ><img
+                  src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FFRPBAPCAR8401_AW_00.jpg"
+                  alt=""
+                  class="img1"
+              /></span>
+            </div>
+            <div class="title">차량용 급속충전기_어피치</div>
+            <div class="price"><span>20,000원</span></div>
+          </li>
+          <li class="cell">
+            <div class="img-box">
+              <span
+                ><img
+                  src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FFRPBRYCAR8400_AW_00.jpg"
+                  alt=""
+                  class="img2"
+              /></span>
+            </div>
+            <div class="title">차량용 거치대_라이언</div>
+            <div class="price"><span>19,000원</span></div>
+          </li>
+          <li class="cell">
+            <div class="img-box">
+              <span
+                ><img
+                  src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2F8809641461544_AW_00.jpg"
+                  alt=""
+                  class="img3"
+              /></span>
+            </div>
+            <div class="title">메모리폼 목베개-리틀 어피치</div>
+            <div class="price"><span>24,000원</span></div>
+          </li>
+          <li class="cell">
+            <div class="img-box">
+              <span
+                ><img
+                  src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FAW_00_shade_TB.jpg"
+                  alt=""
+                  class="img4"
+              /></span>
+            </div>
+            <div class="title">페이스형안대-리틀 튜브</div>
+            <div class="price"><span>12,000원</span></div>
+          </li>
+          <li class="cell">
+            <div class="img-box">
+              <span
+                ><img
+                  src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FAW_00_littleRY_case.jpg"
+                  alt=""
+                  class="img5"
+              /></span>
+            </div>
+            <div class="title">에어팟케이스_리틀라이언</div>
+            <div class="price"><span>15,000원</span></div>
+          </li>
+          <li class="cell">
+            <div class="img-box">
+              <span
+                ><img
+                  src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FFRPBRYHTL0001_AW_00.jpg"
+                  alt=""
+                  class="img6"
+              /></span>
+            </div>
+            <div class="title">핸드타올_별라이언</div>
+            <div class="price"><span>69,000원</span></div>
+          </li>
+          <li class="cell">
+            <div class="img-box">
+              <span
+                ><img
+                  src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FAW_00_neckRY.jpg"
+                  alt=""
+                  class="img7"
+              /></span>
+            </div>
+            <div class="title">후드 목베개-리틀라이언</div>
+            <div class="price"><span>24,000원</span></div>
+          </li>
+          <li class="cell">
+            <div class="img-box">
+              <span
+                ><img
+                  src="https://img1.daumcdn.net/thumb/R300x300/?fname=https%3A%2F%2Ft1.daumcdn.net%2Ffriends%2Fprod%2Fproduct%2FAW_01_passport_AP.jpg"
+                  alt=""
+                  class="img8"
+              /></span>
+            </div>
+            <div class="title">투명여권지갑_리틀 어피치</div>
+            <div class="price"><span>8,000원</span></div>
+          </li>
+        </ul>
+      </div>
+
+      <!--카드-->
+      <div class="card-box con text-align-center">
+        <ul class="row">
+          <li class="cell">
+            <a href="#">
+              <div class="text">충전할 <br />시간이에요</div>
+              <img
+                id="product-card"
+                src="@/assets/images/store/product/dog-card-3.jpg"
+                alt=""
+              />
+            </a>
+          </li>
+          <li class="cell">
+            <a href="#">
+              <div class="text">달콤한 <br />3분 디저트</div>
+              <img
+                id="product-card"
+                src="@/assets/images/store/product/dog-card-2.png"
+                alt=""
+              />
+            </a>
+          </li>
+          <li class="cell">
+            <a href="#">
+              <div class="text">스마트함을 <br />더하는 인싸템</div>
+              <img
+                id="product-card"
+                src="@/assets/images/store/product/dog-card-1.png"
+                alt=""
+              />
+            </a>
+          </li>
+          <li class="cell">
+            <a href="#">
+              <div class="text">충전할 <br />시간이에요</div>
+              <img
+                id="product-card"
+                src="@/assets/images/store/product/dog-card-8.jpg"
+                alt=""
+              />
+            </a>
+          </li>
+        </ul>
+        <!--어피치 리스트-->
+        <div class="con prod-list hot-stuff">
+          <nav class="prod-list-1">
+            <a href="" class="prod-list-bn relative">
+              <div class="top-text">강아지 MBTI<br />DBTI 하러가기!</div>
+              <!-- <div class="bottom-text">잔망둥이 어피치가 한 곳에!</br>어피치를 '픽'해보아요</!br></div> -->
+              <div class="img-box">
+                <img
+                  src="https://t1.daumcdn.net/friends/prod/theme/themeAPEACH_PC.png"
+                  alt=""
+                />
+              </div>
+            </a>
+          </nav>
+        </div>
+      </div>
+
+      <!--모바일 풋터 메뉴-->
+      <div class="mobile-footer mobile">
+        <h3>혹시, 아직 못정했나요?</h3>
+        <nav class="footer-menu">
+          <ul class="row">
+            <li class="cell"><a href="#">전체</a></li>
+            <li class="cell"><a href="#">사료</a></li>
+            <li class="cell"><a href="#">간식 & 건강</a></li>
+            <li class="cell"><a href="#">위생 & 배변</a></li>
+            <li class="cell"><a href="#">미용 & 목욕</a></li>
+            <li class="cell"><a href="#">의류 & 악세사리</a></li>
+            <li class="cell"><a href="#">장난감</a></li>
+            <li class="cell"><a href="#">홈으로...</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="scrollbar" id="style-1">
+        <div class="force-overflow"></div>
       </div>
     </div>
   </div>
@@ -90,6 +321,11 @@ export default {
     ProductCategory,
   },
   setup() {
+    const removeHideFunc = () => {
+      document.getElementById("ulId").classList.remove("hide");
+      document.getElementById("spanId").remove();
+    };
+
     onMounted(() => {
       checkFunc();
     });
@@ -115,6 +351,7 @@ export default {
 
     return {
       checkFunc,
+      removeHideFunc,
     };
   },
 };
