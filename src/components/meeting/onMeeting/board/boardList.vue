87548fce0bd067873ce7a751a3be54545882194c
hyegyeong-kang <rkdgprud96@gmail.com>
2023-04-14 11:19:24 +0900

    add boardList modal in template

--- src/components/meeting/onMeeting/board/boardList.vue
+++ src/components/meeting/onMeeting/board/boardList.vue
@@ -1,12 +1,12 @@
 <template>
-
-
     <div class="onMeeting">
         <div>
-            <button id="register_btn" @click="modalOpen = true" style="background-color: transparent;">게시글작성버튼</button>
+            <button id="register_btn" @click="openModal" style="background-color: transparent;">게시글작성버튼</button>
         </div>
-        <div class="registerModal" v-if="modalOpen == true">
-            <RegisterModal/>
+        <div class="registerModal">
+            <RegisterModal
+                @close="closeModal" v-if="modal"
+            />
         </div>
     </div>
 </template>
@@ -14,24 +14,30 @@
 <script>
 import RegisterModal from '@/components/meeting/onMeeting/board/registerModal/registerModal.vue';
 
-
 export default {
     name: 'BoardList',
     components: {
         RegisterModal
     },
     data() {
-
         return {
-            modalOpen: false,
+            modal: false,
             content: ''
         }
     },
     mounted() {
 
+        // let base = this;
+        // base.openIt = this.$refs.registerModal.openDetailModalFunc;
+
     },
     methods: {
-
+        openModal() {
+            this.modal = true
+        },
+        closeModal() {
+            this.modal = false
+        }
     }
 
 }
