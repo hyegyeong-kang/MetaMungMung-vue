c47e20c40876f6fdf2eccfb7e66187c8bf348783
jeonga-Son <jjeong0106@gmail.com>
2023-04-14 11:43:45 +0900

    add Product Category components

--- src/pages/store/product/detail/_id.vue
+++ src/pages/store/product/detail/_id.vue
@@ -13,93 +13,7 @@
       <div class="bn-line"></div>
       <div class="top-line"></div>
 
-      <div id="pc-categoryMenuWidget" class="_3Te07yM0Z_">
-        <div class="_1bplHci37r">
-          <div class="_3TZha2IPoQ">
-            <div class="kKWaaZzr6M">
-              <div class="_1Do44itO20">
-                <div class="_20owVxSw3I type_white_gnb _1Qz6L4zdcV">
-                  <div class="_1AJ8D2PjS4 _3nO3wKj4-Z">
-                    <div class="_1J2oAxZvAG" style="text-align: center">
-                      <!-- <ul class="_3AV7RVieRB"> -->
-                      <span
-                        class="_1E0Pj6SKQC type_white_gnb _3KXbOehKIC N=a:LNB.best"
-                      >
-                        <a href="/sajopet/best?cp=1" class="_3kz1USn-rs"
-                          >🏆베스트</a
-                        >
-                      </span>
-                      <span
-                        class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                        data-category-menu-key="1006c60a4199496a80fabc6586df7376"
-                      >
-                        <a
-                          href="/sajopet/category/1006c60a4199496a80fabc6586df7376?cp=1"
-                          class="_3HQCww4jR6 _27UBX4PJLi"
-                          >⚡초특가 할인<span class="_3PK0aGa3Hc"></span
-                        ></a>
-                      </span>
-                      <span
-                        class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                        data-category-menu-key="f28a4959a4a843ff9128a580ecccbff1"
-                      >
-                        <a
-                          href="/sajopet/category/f28a4959a4a843ff9128a580ecccbff1?cp=1"
-                          class="_3HQCww4jR6 _27UBX4PJLi"
-                          >🐶강아지 건식<span class="_3PK0aGa3Hc"></span
-                        ></a>
-                      </span>
-                      <span
-                        class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                        data-category-menu-key="b07819b1ddb24db482bd74bc51aa7f32"
-                      >
-                        <a
-                          href="/sajopet/category/b07819b1ddb24db482bd74bc51aa7f32?cp=1"
-                          class="_3HQCww4jR6 _27UBX4PJLi"
-                          >🥫강아지 습식<span class="_3PK0aGa3Hc"></span
-                        ></a>
-                      </span>
-                      <span
-                        class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                        data-category-menu-key="8d4831ae8f4c4605a9ecf3934495bd4f"
-                      >
-                        <a
-                          href="/sajopet/category/8d4831ae8f4c4605a9ecf3934495bd4f?cp=1"
-                          class="_3HQCww4jR6 _27UBX4PJLi"
-                          >🦴강아지 간식&amp;영양제<span
-                            class="_3PK0aGa3Hc"
-                          ></span
-                        ></a>
-                      </span>
-                      <span
-                        class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                        data-category-menu-key="81337ba6dfc14b93aad5f2cef098f0e1"
-                      >
-                        <a
-                          href="/sajopet/category/81337ba6dfc14b93aad5f2cef098f0e1?cp=1"
-                          class="_3HQCww4jR6 _27UBX4PJLi"
-                          >😺고양이 건식<span class="_3PK0aGa3Hc"></span
-                        ></a>
-                      </span>
-                      <span
-                        class="_2jm5JW3D5W type_white_gnb YI_nVHGI_0 N=a:lca.depth1"
-                        data-category-menu-key="672ece7ec6e14aef957bb35f8725d76f"
-                      >
-                        <a
-                          href="/sajopet/category/672ece7ec6e14aef957bb35f8725d76f?cp=1"
-                          class="_3HQCww4jR6 _27UBX4PJLi"
-                          >🥫고양이 습식<span class="_3PK0aGa3Hc"></span
-                        ></a>
-                      </span>
-                      <!-- </ul> -->
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ProductCategory />
 
       <div class="shCMSshop">
         <div class="shopView">
@@ -275,7 +189,13 @@
 </template>
 
 <script>
-export default {};
+import ProductCategory from "@/components/store/product/ProductCategory.vue";
+
+export default {
+  components: {
+    ProductCategory,
+  },
+};
 </script>
 
 <style scoped>
