2a67c6798fcdf3ee7c1d2ac5cd43151e8e290595
hyegyeong-kang <rkdgprud96@gmail.com>
2023-04-15 17:18:26 +0900

    add replyList.vue, createReply.vue

---
+++ src/components/meeting/onMeeting/board/reply/createReply.vue
@@ -0,0 +1,57 @@
+<!--댓글쓰고 댓글달기 버튼있는 곳 -->
+<template>
+  <div>
+    <hr>
+    <textarea
+      placeholder="댓글을 입력하세요"
+      v-model="context"
+    ></textarea>
+    <button @click="createComment">보내기</button>
+  </div>
+</template>
+
+<script>
+// import data from "@/data"
+export default {
+    name:"PrCommentCreate",
+    props:{
+        contentId:Number,
+        reload:Function
+    },
+    data(){
+        return{
+        context:'',
+        replyList: []
+        }
+    },
+    methods:{
+        createComment(){
+            const replyObj = {
+                comment_id: replyList[replyList.length - 1].comment_id + 1,
+                user_id: 1,
+                content_id: this.contentId,
+                context: this.context,
+                created_at: '2019-03-29 14:11:11',
+                updated_at: null
+            }
+            replyList.push(replyObj);
+        // data.Comment.push(
+        //     {
+        //         comment_id: data.Comment[data.Comment.length - 1].comment_id + 1,
+        //         user_id: 1,
+        //         content_id: this.contentId,
+        //         context: this.context,
+        //         created_at: '2019-03-29 14:11:11',
+        //         updated_at: null
+        //     }
+        // )
+        this.reload();
+        console.log(replyObj)
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/meeting/onMeeting/board/reply/replyList.vue
@@ -0,0 +1,49 @@
+<!--댓글 속성: 작성자, 작성일자 및 시간, 내용 / + 수정 삭제  -->
+<template>
+  <div class="commentItem">
+    <div>{{ 이른 }}</div>
+    <div>{{ 게시일 }}</div>
+    <div>{{ 내용ㅇ }}</div>
+    <b-button @click="updateComment">수정</b-button>
+    <b-button @click="deleteComment">삭제</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"PrCommentListItem",
+  props:{
+    commentItem:Object,
+    reload:Function
+  },
+  data(){
+    return{
+      name:'',
+    }
+  },
+  created(){
+  //  this.name = data.User.filter(item => item.user_id === this.commentItem.user_id)[0].name
+  },
+  methods:{
+    updateComment(){
+
+    },
+    deleteComment(){
+      const commentIdx = this.commentItem.comment_id;
+     // const deleteArrayId = data.Comment.findIndex(item => item.comment_id === commentIdx);
+     // data.Comment.splice(deleteArrayId, 1);
+      this.reload();
+
+    }
+  }
+
+}
+</script>
+
+<style>
+.commentItem{
+  border:1px solid #000;
+  margin-bottom:10px;
+  padding:10px;
+}
+</style>
