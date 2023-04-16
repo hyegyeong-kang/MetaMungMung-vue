17311f499cbf4ba8cacde366c6f19a7321f72c22
hyegyeong-kang <rkdgprud96@gmail.com>
2023-04-16 19:35:05 +0900

    add createReply.vue

--- src/components/meeting/onMeeting/board/reply/createReply.vue
+++ src/components/meeting/onMeeting/board/reply/createReply.vue
@@ -1,12 +1,16 @@
 <!--댓글쓰고 댓글달기 버튼있는 곳 -->
 <template>
-  <div>
-    <hr>
-    <textarea
-      placeholder="댓글을 입력하세요"
-      v-model="context"
-    ></textarea>
-    <button @click="createComment">보내기</button>
+  <div class="reply">
+      <span>
+          <textarea
+            placeholder="댓글을 입력하세요"
+            v-model="context"
+            style="border: 1px solid;border-radius: 2em;color:#C0C0C0"
+            ></textarea>
+      </span>
+      <span>
+          <button @click="createComment" style="border: 1px solid;border-radius: 2em;color:#fff">보내기</button>
+      </span>
   </div>
 </template>
 
@@ -52,6 +56,20 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.reply
+    textarea{
+        width: 80%;
+        height: 32px;
+        resize: none;
+        overflow:hidden;
+        font-size: 15px;
+        vertical-align:middle;
+    }
+    button {
+        width: 20%;
+        vertical-align:middle;
+        background-color: #87cefa;
+	    color: black;
+    }
 </style>
