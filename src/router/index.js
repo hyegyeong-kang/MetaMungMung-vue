e7b01006209de681839bfd49e364b07e6c123c16
lhyeonj120 <isk04387@gmail.com>
2023-04-17 00:54:20 +0900

    add Order files

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,19 @@ import Login from "../pages/member/login.vue";
 import Modify from "../pages/member/modify.vue";
 import OffMeeting from "../pages/meeting/offMeeting/index.vue";
 import OnMeeting from "../pages/meeting/onMeeting/index.vue";
-import OnMeetingCreate from '../pages/meeting/onMeeting/create/index.vue';
-import RegisterModal from '../pages/meeting/onMeeting/board/index.vue';
+import OnMeetingCreate from "../pages/meeting/onMeeting/create/index.vue";
+import RegisterModal from "../pages/meeting/onMeeting/board/index.vue";
 import ProductList from "../pages/store/product/index.vue";
 import ProductDetail from "../pages/store/product/detail/_id.vue";
 import ProductReviews from "../pages/store/product/review/index.vue";
 import OnMeetingDetail from "../pages/meeting/onMeeting/_id.vue";
 import OnMeetingSearch from "../pages/meeting/onMeeting/search/index.vue";
+import OnMeetingDetailSetting from "../pages/meeting/onMeeting/modify/index.vue";
+import OnMeetingModify from "../pages/meeting/onMeeting/modify/_id.vue";
+import Order from "../pages/store/order/index.vue";
+import OrderSuccess from "../pages/store/order/success/index.vue";
+import OrdersList from "../pages/store/order/list/index.vue";
+import OrderDetail from "../pages/store/order/_id.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -80,6 +86,36 @@ const router = createRouter({
       path: "/onMeeting/search",
       name: "OnMeetingSearch",
       component: OnMeetingSearch
+    },
+    {
+      path: "/onMeeting/:id/setting",
+      name: "OnMeetingDetailSetting",
+      component: OnMeetingDetailSetting
+    },
+    {
+      path: "/onMeeting/:id/modify",
+      name: "OnMeetingModify",
+      component: OnMeetingModify
+    },
+    {
+      path: "/store/order",
+      name: "Order",
+      component: Order
+    },
+    {
+        path: "/store/order/success",
+        name: "OrderSuccess",
+        component: OrderSuccess
+    },
+    {
+        path: "/store/order/list",
+        name: "OrderList",
+        component: OrdersList
+    },
+    {
+        path: "/store/order/:id",
+        name: "OrderDetail",
+        component: OrderDetail
     }
   ],
   scrollBehavior(){
